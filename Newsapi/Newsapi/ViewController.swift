bad95ded880a9382f78f2d7f279bc41aa399beaf
Emre Çiftçi <emre.ciftci@vakifbank.com.tr>
2019-06-15 15:14:54 +0300

    initial commit
    - Created XCode project

---
+++ Newsapi/Newsapi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsapi
+//
+//  Created by Emre Çiftçi on 15.06.2019.
+//  Copyright © 2019 Emre Çiftçi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
